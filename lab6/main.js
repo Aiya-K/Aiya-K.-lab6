96fe43e4b499e40abee909de54d3b49b137dc44a
Aiya-K <122736019+Aiya-K@users.noreply.github.com>
2023-03-02 00:02:34 +0600

    Add files via upload

---
+++ lab6/main.js
@@ -0,0 +1,14 @@
+//3.	Write a function that finds an area of a circle given its radius (PI=3.14).
+let r= 5; 
+const PI = 3.14;
+let area = Math.pow(r,2)*PI; 
+document.write (area); 
+//10.	Write a function to check whether two given numbers are equal or not.
+let a= 8;
+let b= 7;
+if (a!=b) {
+    console.log('not equal'); 
+}
+else if (a=b){
+    console.log('equal'); 
+}
